e87d5963085cf06d0c7a16e0aabfb843d50196ad
JohnTsai <johntsai.work@gmail.com>
2015-11-20 23:37:37 +0800

    Modify the demo Activity to show the NumberProgressBar better.

--- demo/src/main/java/com/daimajia/numberprogressbar/example/MainActivity.java
+++ demo/src/main/java/com/daimajia/numberprogressbar/example/MainActivity.java
@@ -2,6 +2,7 @@ package com.daimajia.numberprogressbar.example;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -13,30 +14,44 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 
-public class MainActivity extends ActionBarActivity implements OnProgressBarListener {
-    private Timer timer;
 
-    private NumberProgressBar bnp;
+public class MainActivity extends ActionBarActivity implements OnProgressBarListener {
+
+    private Timer timer;
+    private TimerTask timerTask;
+
+    private NumberProgressBar[] npbArray = new NumberProgressBar[8];
+    private int[] ids = {R.id.numberbar1, R.id.numberbar2, R.id.numberbar3, R.id.numberbar4, R.id.numberbar5, R.id.numberbar6, R.id.numberbar7, R.id.numberbar8};
+    private int currentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bnp = (NumberProgressBar)findViewById(R.id.numberbar1);
-        bnp.setOnProgressBarListener(this);
+        for (int i = 0; i < ids.length; i++) {
+            npbArray[i] = (NumberProgressBar) findViewById(ids[i]);
+            npbArray[i].setOnProgressBarListener(this);
+        }
+
         timer = new Timer();
-        timer.schedule(new TimerTask() {
+        runTimer();
+    }
+
+    private void runTimer() {
+        timerTask = new TimerTask() {
             @Override
             public void run() {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        bnp.incrementProgressBy(1);
+                        npbArray[currentIndex].incrementProgressBy(1);
                     }
                 });
             }
-        }, 1000, 100);
+        };
+        if (timer != null)
+            timer.schedule(timerTask, 1000, 100);
     }
 
     @Override
@@ -52,23 +67,68 @@ public class MainActivity extends ActionBarActivity implements OnProgressBarList
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_reset) {
+            resetNPB();
+            return true;
+        }else if(id==R.id.action_stop){
+            stopNPB();
+            return true;
+        }else if(id==R.id.action_start){
+            startNPB();
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
+    private void startNPB() {
+        runTimer();
+    }
+
+    private void stopNPB() {
+        if(timerTask!=null){
+            timerTask.cancel();
+            timerTask = null;
+        }
+    }
+
+    private void resetNPB() {
+        for (int i = 0; i < npbArray.length; i++) {
+            npbArray[i].setProgress(10 * (i + 1));
+        }
+        currentIndex = 0;
+        timerTask.cancel();
+        timerTask = null;
+        runTimer();
+    }
+
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        timer.cancel();
+    protected void onPause() {
+        super.onPause();
+        if (timerTask != null) {
+            timerTask.cancel();
+            timerTask = null;
+        }
+        if (timer != null) {
+            timer.cancel();
+            timer = null;
+        }
+        currentIndex = 0;
     }
 
     @Override
     public void onProgressChange(int current, int max) {
-        if(current == max) {
-            Toast.makeText(getApplicationContext(), getString(R.string.finish), Toast.LENGTH_SHORT).show();
-            bnp.setProgress(0);
+        if (currentIndex < npbArray.length - 1) {
+            if (current == max) {
+                currentIndex++;
+                timerTask.cancel();
+                timerTask = null;
+                runTimer();
+            }
+        } else {
+            if (current == max) {
+                Toast.makeText(getApplicationContext(), getString(R.string.finish), Toast.LENGTH_SHORT).show();
+                resetNPB();
+            }
         }
     }
 }
