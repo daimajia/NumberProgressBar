773b07941e4dc9adaa861299ee766479607701e8
jeho <taz@ylabs.kr>
2018-05-15 18:18:40 +0900

    add setTypface ex.) numberProgressBar.setTypeface(typeface) fix #57 #60

--- demo/src/main/java/com/daimajia/numberprogressbar/example/MainActivity.java
+++ demo/src/main/java/com/daimajia/numberprogressbar/example/MainActivity.java
@@ -1,7 +1,7 @@
 package com.daimajia.numberprogressbar.example;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -13,7 +13,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 
-public class MainActivity extends ActionBarActivity implements OnProgressBarListener {
+public class MainActivity extends AppCompatActivity implements OnProgressBarListener {
     private Timer timer;
 
     private NumberProgressBar bnp;
@@ -23,7 +23,7 @@ public class MainActivity extends ActionBarActivity implements OnProgressBarList
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bnp = (NumberProgressBar)findViewById(R.id.numberbar1);
+        bnp = (NumberProgressBar) findViewById(R.id.numberbar1);
         bnp.setOnProgressBarListener(this);
         timer = new Timer();
         timer.schedule(new TimerTask() {
@@ -66,7 +66,7 @@ public class MainActivity extends ActionBarActivity implements OnProgressBarList
 
     @Override
     public void onProgressChange(int current, int max) {
-        if(current == max) {
+        if (current == max) {
             Toast.makeText(getApplicationContext(), getString(R.string.finish), Toast.LENGTH_SHORT).show();
             bnp.setProgress(0);
         }
